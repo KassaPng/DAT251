fc19f49dadf83337440923f3c802435bc766b83d
Tore Berven <tore.berven@student.uib.no>
2021-02-22 17:54:47 +0100

    Renamed Test to TestMain to resolve conflict
    Added method helloWorld() in Main
    Added method for testing helloWorld() in TestMain
    
    This basic test is meant to test
    that the CI pipeline is working.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+
+
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+
+
+
+    @Test
+    void testHelloWorld() {
+        Main main = new Main();
+        assert(main.helloWorld().equals("Hello World!"));
+    }
+
+
+
+}
