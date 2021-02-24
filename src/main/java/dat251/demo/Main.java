66cde0131c92166327fcedc2137123d157ed583b
Kristian Haga <kristian_haga@hotmail.no>
2021-02-24 10:46:24 +0100

    Added a controller class with a simple request

--- src/main/java/dat251/demo/Main.java
+++ src/main/java/dat251/demo/Main.java
@@ -1,16 +1,19 @@
 package dat251.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
-        public static void main(String[] args) {
-            SpringApplication.run(Main.class, args);
-        }
 
-        public static String helloWorld() {
-            return "Hello World!";
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
+    public static String helloWorld() {
+        return "Hello World!";
+    }
 }
