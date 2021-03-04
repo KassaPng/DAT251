3cdfdd3a200df037369df3033cb3496988890678
Tore Berven <tore.berven@student.uib.no>
2021-03-04 18:50:11 +0100

    Created User and Group entities

--- src/main/java/entities/Group.java
+++ src/main/java/entities/Group.java
@@ -1,4 +1,40 @@
 package entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "group")
 public class Group {
+    @Id
+    private long id;
+    private String groupName;
+
+    public Group(String groupName) {
+        if (groupName.length() < 3) {
+            throw new IllegalArgumentException("Group name must be at least three characters long");
+        }
+        this.groupName = groupName;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[Id='%d', " +
+                        "groupName='%s']",
+                id, groupName
+        );
+    }
 }
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,15 +1,53 @@
 package entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @Column(unique = true)
     private String userName;
+    private String name;
+
+
+    //    @JsonIgnore // Enable to prevent password from being used in output.
+ //   @Setter(AccessLevel.NONE)
+    private String passwordAsHash;
+
+
+
+    public User(String name, String userName, String password) {
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("Name was too short");
+        }
+        this.name = name;
+        this.userName = userName;
+        this.passwordAsHash = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[Id='%d', " +
+                        "name= '%s', " +
+                        "userName='%s']",
+                id, name, userName
+        );
+    }
 }
