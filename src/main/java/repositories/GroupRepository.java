4389e60fc5a00bf6a1f3ecb36bd772f501a676ef
Tore Berven <tore.berven@student.uib.no>
2021-03-04 18:51:11 +0100

    Added method for finding group from ID

--- src/main/java/repositories/GroupRepository.java
+++ src/main/java/repositories/GroupRepository.java
@@ -10,6 +10,8 @@ public interface GroupRepository extends CrudRepository<Group, String> {
 
     List<Group> findByUserName(String userName); // All groups that a user is participating in
 
+    Group findById(long id);
+
     List<Group> findByGroupName(String groupName); // return a list if the group name is not unique --> change to a single Group if it is
 
     List<Group> findByTag(Tag tag); // All groups containing specified tag
