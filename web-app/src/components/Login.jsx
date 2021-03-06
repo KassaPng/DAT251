0d4a0b8ff3b3e365de40ed8168822a7d939f0924
Tore Berven <tore.berven@student.uib.no>
2021-03-06 15:12:52 +0100

    Commented out unused code to pass CI test
    GitHub Actions CI treat warnings as errors,
    and is probably the reason why the build fails
    in CI while it otherwise works fine.

--- web-app/src/components/Login.jsx
+++ web-app/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Nav from 'react-bootstrap/Nav'
+//import Nav from 'react-bootstrap/Nav'
 import "../containers/Login.css";
 
 function Login() {
--- web-app/src/components/Register.jsx
+++ web-app/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState, setState } from "react";
+import React, { useState } from "react"; //import React, { useState, setState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../containers/Login.css";
