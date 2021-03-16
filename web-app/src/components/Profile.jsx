b27a21110bd6793fb00d2b18c484065e8aa848a5
Dovydas <spdoviss@gmail.com>
2021-03-16 16:19:58 +0100

    added connection to back-end and implemented cookies

--- web-app/src/components/Profile.jsx
+++ web-app/src/components/Profile.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { getSessionCookie } from "../Cookies/Session.js";
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'test',
     }
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return true;
   }
 
   getUserData = () => {
@@ -14,14 +18,23 @@ class Profile extends React.Component {
 
     xhr.addEventListener('load', () => {
       const data = xhr.responseText;
+      console.log(data);
+      this.setState({
+        name: data.name,
+        userName: data.userName,
+        groups: data.groups
+      });
     })
-    const URL = 'http://localhost:8080/users/';
+    const URL = 'http://localhost:8080/users/' + this.state.email;
 
     xhr.open('GET', URL);
     xhr.send(URL);
   }
   componentDidMount() {
-
+    this.setState({"email": getSessionCookie().email
+    }, () => {
+      this.getUserData();
+    });
   }
 
 
@@ -33,7 +46,7 @@ class Profile extends React.Component {
             <div class="col-lg-5">
               <h1 class="font-weight-light">Profile Page</h1>
               <p>
-                {this.state.name}
+                {this.state.userName}
               </p>
             </div>
           </div>
