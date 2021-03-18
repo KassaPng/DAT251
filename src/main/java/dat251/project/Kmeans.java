83c06413e8336d9a3cb633c250d111f853de1fba
Simen <Simen.Carstensen@student.uib.no>
2021-03-18 15:45:16 +0100

    formatting

--- src/main/java/dat251/project/Kmeans.java
+++ src/main/java/dat251/project/Kmeans.java
@@ -21,7 +21,6 @@ public class Kmeans {
     }
 
 
-
     private static Random rand = new Random(5);//TODO: remove seed after testing
     private static int maxIterations = 1000;
     private static ArrayList<String> abilities; // TODO: Placeholder (List of all ability-names)
@@ -33,7 +32,7 @@ public class Kmeans {
         ArrayList<Centroid> centroids = new ArrayList<>();
 
         //create k random centroids
-        for(int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) {
             Map<String, Double> randomMap = new HashMap<>();
             for (String ability : abilities) {
                 randomMap.put(ability, (double) (rand.nextInt(10)));
@@ -41,19 +40,15 @@ public class Kmeans {
             centroids.add(new Centroid(randomMap));
         }
 
-
-
-        for(int i = 0; i < maxIterations; i++) {
-
-
-            for(User u : users) {
+        for (int i = 0; i < maxIterations; i++) {
+            for (User u : users) {
                 //Assign each user to a cluster
                 Centroid closest = closestCentroid(centroids, u);
                 setToCluster(clusters, u, closest);
             }
             boolean done = clusters.equals(lastClusters);
             lastClusters = clusters;
-            if(i == maxIterations-1 || done) {
+            if (i == maxIterations - 1 || done) {
                 break;
             }
 
@@ -65,18 +60,15 @@ public class Kmeans {
             centroids = newCentroids;
             clusters = new HashMap<>();
         }
-
         return lastClusters;
     }
-
-
 
 
     //euclidean distance between two attribute lists.
     private static double distance(Map<String, Double> a1, Map<String, Double> a2) {
         double sum = 0;
-        for(String key : a1.keySet()) {
-            sum += Math.pow(a1.get(key) - a2.get(key),2); //This assumes that every user will have the same attributes in their list
+        for (String key : a1.keySet()) {
+            sum += Math.pow(a1.get(key) - a2.get(key), 2); //This assumes that every user will have the same attributes in their list
         }
         return Math.sqrt(sum);
     }
@@ -85,9 +77,9 @@ public class Kmeans {
     private static Centroid closestCentroid(ArrayList<Centroid> centroids, User user) {
         double minDist = Double.MAX_VALUE;
         Centroid closest = null;
-        for(Centroid centroid : centroids) {
+        for (Centroid centroid : centroids) {
             double dist = distance(centroid.coords, user.attributes);
-            if(dist < minDist) {
+            if (dist < minDist) {
                 minDist = dist;
                 closest = centroid;
             }
@@ -96,29 +88,28 @@ public class Kmeans {
     }
 
     private static void setToCluster(Map<Centroid, ArrayList<User>> clusters, User user, Centroid centroid) {
-        if(clusters.get(centroid) == null) {
+        if (clusters.get(centroid) == null) {
             clusters.put(centroid, new ArrayList<>());
         }
         clusters.get(centroid).add(user);
     }
 
 
-
     private static Centroid calcNewCentroid(Centroid centroid, ArrayList<User> users) {
-        if(users == null) {
+        if (users == null) {
             return centroid;
         }
         Map<String, Double> average = centroid.coords;
-        for(String ab : abilities) {
+        for (String ab : abilities) {
             average.put(ab, 0.0); // set all centroid abilities to 0.0
         }
-        for(User u : users) {
-            for(String ab : abilities) {
+        for (User u : users) {
+            for (String ab : abilities) {
                 double current = average.get(ab);
                 centroid.coords.put(ab, current + u.attributes.get(ab)); //increment average values by user values
             }
         }
-        for(String ab : abilities) {
+        for (String ab : abilities) {
             double current = average.get(ab);
             average.put(ab, current / users.size()); // find average by dividing each attribute sum by total size of cluster
         }
