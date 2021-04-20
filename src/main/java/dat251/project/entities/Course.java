16f2b8ea56ca75b1f8fb561b984d1f70f9a77e1f
Tore Berven <tore.berven@student.uib.no>
2021-04-20 19:16:31 +0200

    Courses now have a description and the name of the educational institution teaching the course.

--- src/main/java/dat251/project/entities/Course.java
+++ src/main/java/dat251/project/entities/Course.java
@@ -9,22 +9,30 @@ import javax.persistence.Table;
 public class Course {
 
     private static final int MINIMUM_COURSE_NAME_LENGTH = 3;
+    static final String DEFAULT_NAME_OF_EDUCATIONAL_INSTITUTION = "None";
+    static final String DEFAULT_COURSE_DESCRIPTION = "This course has not yet defined a description.";
 
     @Id
     private long id;
     private String name;
-
+    private String institutionName; // Name of educational institution where the course is taught.
+    private String description;
 
 
 
 
     public Course() {}
 
-    public Course(String name) {
-        if (name.length() < MINIMUM_COURSE_NAME_LENGTH) {
+    public Course(String courseName, String nameOfInstitutionTeachingThisCourse, String description) {
+        if (courseName.length() < MINIMUM_COURSE_NAME_LENGTH) {
             throw new IllegalArgumentException("Course name was too short");
         }
-        this.name = name;
+        this.name = courseName;
+        this.institutionName = (nameOfInstitutionTeachingThisCourse == null
+                || nameOfInstitutionTeachingThisCourse.isEmpty()) ?
+                DEFAULT_NAME_OF_EDUCATIONAL_INSTITUTION : nameOfInstitutionTeachingThisCourse;
+        this.description = (description == null || description.isEmpty()) ? DEFAULT_COURSE_DESCRIPTION : description;
+
     }
 
     public long getId() {
@@ -41,5 +49,21 @@ public class Course {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- src/test/java/dat251/project/entities/CourseTest.java
+++ src/test/java/dat251/project/entities/CourseTest.java
@@ -1,5 +1,6 @@
 package dat251.project.entities;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,18 +13,81 @@ class CourseTest {
     @BeforeEach
     void setUp() {
         String name = "DAT251";
-        course = new Course(name);
+        String nameOfInstitution = "UiB";
+        String courseDescription = "Description";
+        course = new Course(name, nameOfInstitution, courseDescription);
     }
 
     @Test
-    void courseShouldHaveANameOfAtLeastThreeCharacters() {
+    void coursesShouldHaveANameConsistingOfAtLeastThreeCharacters() {
         String tooShortName = "AB";
-        assertThrows(IllegalArgumentException.class, () -> new Course(tooShortName));
+        assertThrows(IllegalArgumentException.class, () -> new Course(tooShortName, "", ""));
         String longEnoughName = "ABC";
-        assertDoesNotThrow(() -> new Course(longEnoughName));
+        assertDoesNotThrow(() -> new Course(longEnoughName, "", ""));
+    }
+
+    @Test
+    void coursesShouldBeTaughtAtAnEducationalInstitution() {
+        Course course1 = new Course("ABC", "", "");
+        assertFalse(course1.getInstitutionName().isEmpty());
+        assertEquals(Course.DEFAULT_NAME_OF_EDUCATIONAL_INSTITUTION, course1.getInstitutionName());
+
+        String institutionName = "UiB";
+        Course course2 = new Course("ABC", institutionName, "");
+        assertEquals(institutionName, course2.getInstitutionName());
+    }
+
+    @Test
+    void coursesShouldHaveACourseDescription() {
+        Course course1 = new Course("ABC", "", "");
+        assertFalse(course1.getDescription().isEmpty());
+        assertEquals(Course.DEFAULT_COURSE_DESCRIPTION, course1.getDescription());
+
+        String description = "Course for teaching software development methods and best practices";
+        Course course2 = new Course("DAT251", "UiB", description);
+        assertEquals(description, course2.getDescription());
+    }
+
+
+
+    @Test
+    void coursesShouldHaveReferencesToAllGroupsRelatedToThatCourse() {
+
+
+      //  course.getRelatedGroups();
+
+
     }
 
 
 
 
+
+
+
+
+
+/*
+
+ @Test
+    void a() {
+
+    }
+    @Test
+    void a() {
+
+    }
+    @Test
+    void a() {
+
+    }
+    @Test
+    void a() {
+
+    }
+    @Test
+    void a() {
+
+    }
+//*/
 }
